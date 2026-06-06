f6f0c8bb63ec8ff649cbf751e1556ccab6ca4ae5
shivrajpathadedev-dev <shivrajpathade.dev@gmail.com>
2026-06-06 16:35:50 +0530

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tdform-task1';
+  @ViewChild('studentform') studentform!:NgForm
+genders: string[] = ['Male', 'Female'];
+  onSubmit(){
+    if(this.studentform.valid){
+      this.studentform.form.markAllAsTouched()
+    }
+    this.studentform.reset()
+  }
+onlyNumbers(event: KeyboardEvent) {
+  const key = event.key;
+
+  if (!/^[0-9]$/.test(key)) {
+    event.preventDefault();
+  }
 }
+
+onMobileInput(event: any) {
+  event.target.value = event.target.value.replace(/[^0-9]/g, '');
+}
+}
+
+
+
+
+
